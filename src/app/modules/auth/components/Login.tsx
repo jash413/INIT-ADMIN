5a075e21f3c49bedc5aa5e18c57c704107028a4c
jash413 <99473238+jash413@users.noreply.github.com>
2024-06-27 17:00:37 +0530

    feat: OTP-LOGIN

--- src/app/modules/auth/components/Login.tsx
+++ src/app/modules/auth/components/Login.tsx
@@ -1,10 +1,12 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import * as Yup from 'yup'
 import clsx from 'clsx'
-import {Link} from 'react-router-dom'
-import {useFormik} from 'formik'
-import {getUserByToken, login} from '../core/_requests'
-import {useAuth} from '../core/Auth'
+import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
+import { getUserByToken, login } from '../core/_requests'
+import { useAuth } from '../core/Auth'
+import axios from 'axios'
+const API_URL = import.meta.env.VITE_APP_THEME_API_URL;
 
 const loginSchema = Yup.object().shape({
   email: Yup.string()
@@ -16,33 +18,68 @@ const loginSchema = Yup.object().shape({
     .min(3, 'Minimum 3 symbols')
     .max(50, 'Maximum 50 symbols')
     .required('Password is required'),
+  mobile: Yup.string()
+    .min(10, 'Minimum 10 digits')
+    .max(10, 'Maximum 10 digits')
+    .required('Mobile number is required'),
+  otp: Yup.string()
+    .min(6, 'Minimum 6 digits')
+    .max(6, 'Maximum 6 digits'),
 })
 
 const initialValues = {
   email: 'devansh@example.com',
   password: '1234',
+  mobile: '',
+  otp: '',
 }
 
 export function Login() {
   const [loading, setLoading] = useState(false)
-  const {saveAuth, setCurrentUser} = useAuth()
+  const [otpSent, setOtpSent] = useState(false)
+  const [generatedOtp, setGeneratedOtp] = useState('')
+  const { saveAuth, setCurrentUser } = useAuth()
 
   const formik = useFormik({
     initialValues,
     validationSchema: loginSchema,
-    onSubmit: async (values, {setStatus, setSubmitting}) => {
+    onSubmit: async (values, { setStatus, setSubmitting }) => {
       setLoading(true)
-      try {
-        let {data} = await login(values.email, values.password)
-        saveAuth(data.data)
-        data = await getUserByToken(data.data.accessToken)
-        setCurrentUser(data.data.data)
-      } catch (error) {
-        console.error(error)
-        saveAuth(undefined)
-        setStatus('The login details are incorrect')
-        setSubmitting(false)
-        setLoading(false)
+      if (!otpSent) {
+        try {
+          const response = await axios.post(`${API_URL}/api/admins/otp`, {
+            phoneNumber: values.mobile,
+          })
+          setGeneratedOtp(response.data.generatedOTP)
+          console.log(response.data.generatedOTP)
+          setOtpSent(true)
+          setLoading(false)
+        } catch (error) {
+          console.error(error)
+          setStatus('Failed to send OTP')
+          setSubmitting(false)
+          setLoading(false)
+        }
+      } else {
+        if (values.otp === generatedOtp) {
+          try {
+            let { data } = await login(values.email, values.password)
+            saveAuth(data.data)
+            data = await getUserByToken(data.data.accessToken)
+            setCurrentUser(data.data.data)
+          } catch (error) {
+            console.error(error)
+            saveAuth(undefined)
+            setStatus('The login details are incorrect')
+            setSubmitting(false)
+            setLoading(false)
+          }
+        } else {
+          setStatus('Invalid OTP')
+          setSubmitting(false)
+          setLoading(false)
+          setOtpSent(false)
+        }
       }
     },
   })
@@ -54,12 +91,10 @@ export function Login() {
       noValidate
       id='kt_login_signin_form'
     >
-      {/* begin::Heading */}
       <div className='text-center mb-11'>
         <h1 className='text-gray-900 fw-bolder mb-3'>Sign In</h1>
         <div className='text-gray-500 fw-semibold fs-6'>TO ACCESS DASHBOARD</div>
       </div>
-      {/* begin::Heading */}
 
       {formik.status ? (
         <div className='mb-lg-15 alert alert-danger'>
@@ -68,13 +103,11 @@ export function Login() {
       ) : (
         <div className='mb-10 bg-light-info p-8 rounded'>
           <div className='text-info'>
-            Use account <strong>devansh@example.com</strong> and password <strong>1234</strong> to
-            continue.
+            Use account <strong>devansh@example.com</strong> and password <strong>1234</strong> to continue.
           </div>
         </div>
       )}
 
-      {/* begin::Form group */}
       <div className='fv-row mb-8'>
         <label className='form-label fs-6 fw-bolder text-gray-900'>Email</label>
         <input
@@ -82,10 +115,8 @@ export function Login() {
           {...formik.getFieldProps('email')}
           className={clsx(
             'form-control bg-transparent',
-            {'is-invalid': formik.touched.email && formik.errors.email},
-            {
-              'is-valid': formik.touched.email && !formik.errors.email,
-            }
+            { 'is-invalid': formik.touched.email && formik.errors.email },
+            { 'is-valid': formik.touched.email && !formik.errors.email },
           )}
           type='email'
           name='email'
@@ -97,10 +128,29 @@ export function Login() {
           </div>
         )}
       </div>
-      {/* end::Form group */}
 
-      {/* begin::Form group */}
-      <div className='fv-row mb-3'>
+      <div className='fv-row mb-8'>
+        <label className='form-label fs-6 fw-bolder text-gray-900'>Mobile Number</label>
+        <input
+          placeholder='Mobile Number'
+          {...formik.getFieldProps('mobile')}
+          className={clsx(
+            'form-control bg-transparent',
+            { 'is-invalid': formik.touched.mobile && formik.errors.mobile },
+            { 'is-valid': formik.touched.mobile && !formik.errors.mobile },
+          )}
+          type='text'
+          name='mobile'
+          autoComplete='off'
+        />
+        {formik.touched.mobile && formik.errors.mobile && (
+          <div className='fv-plugins-message-container'>
+            <span role='alert'>{formik.errors.mobile}</span>
+          </div>
+        )}
+      </div>
+
+      <div className='fv-row mb-8'>
         <label className='form-label fw-bolder text-gray-900 fs-6 mb-0'>Password</label>
         <input
           type='password'
@@ -108,12 +158,8 @@ export function Login() {
           {...formik.getFieldProps('password')}
           className={clsx(
             'form-control bg-transparent',
-            {
-              'is-invalid': formik.touched.password && formik.errors.password,
-            },
-            {
-              'is-valid': formik.touched.password && !formik.errors.password,
-            }
+            { 'is-invalid': formik.touched.password && formik.errors.password },
+            { 'is-valid': formik.touched.password && !formik.errors.password },
           )}
         />
         {formik.touched.password && formik.errors.password && (
@@ -124,21 +170,37 @@ export function Login() {
           </div>
         )}
       </div>
-      {/* end::Form group */}
 
-      {/* begin::Wrapper */}
+      {otpSent && (
+        <div className='fv-row mb-8'>
+          <label className='form-label fs-6 fw-bolder text-gray-900'>OTP</label>
+          <input
+            placeholder='OTP'
+            {...formik.getFieldProps('otp')}
+            className={clsx(
+              'form-control bg-transparent',
+              { 'is-invalid': formik.touched.otp && formik.errors.otp },
+              { 'is-valid': formik.touched.otp && !formik.errors.otp },
+            )}
+            type='text'
+            name='otp'
+            autoComplete='off'
+          />
+          {formik.touched.otp && formik.errors.otp && (
+            <div className='fv-plugins-message-container'>
+              <span role='alert'>{formik.errors.otp}</span>
+            </div>
+          )}
+        </div>
+      )}
+
       <div className='d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8'>
         <div />
-
-        {/* begin::Link */}
         <Link to='/auth/forgot-password' className='link-primary'>
           Forgot Password ?
         </Link>
-        {/* end::Link */}
       </div>
-      {/* end::Wrapper */}
 
-      {/* begin::Action */}
       <div className='d-grid mb-10'>
         <button
           type='submit'
@@ -148,14 +210,13 @@ export function Login() {
         >
           {!loading && <span className='indicator-label'>Continue</span>}
           {loading && (
-            <span className='indicator-progress' style={{display: 'block'}}>
+            <span className='indicator-progress' style={{ display: 'block' }}>
               Please wait...
               <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
             </span>
           )}
         </button>
       </div>
-      {/* end::Action */}
     </form>
   )
 }
