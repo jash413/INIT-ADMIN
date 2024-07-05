b6ee8f003c5b0839cbe5f7d60840658c486e2b51
jash413 <99473238+jash413@users.noreply.github.com>
2024-07-05 18:31:11 +0530

    fix: subscription

--- src/app/modules/subscriptions/subscriptions-list/components/header/SubscriptionListToolbar.tsx
+++ src/app/modules/subscriptions/subscriptions-list/components/header/SubscriptionListToolbar.tsx
@@ -1,35 +1,143 @@
 import { KTIcon } from "../../../../../../_metronic/helpers";
 import { useListView } from "../../core/ListViewProvider";
 import { SubscriptionsListFilter } from "./SubscriptionsListFilter";
-import { useQueryResponseData, useQueryResponseLoading } from "../../core/QueryResponseProvider";
+import {
+  useQueryResponseData,
+  useQueryResponseLoading,
+} from "../../core/QueryResponseProvider";
 import { FC, useEffect, useState } from "react";
 import { Subscription } from "../../core/_models";
+import {
+  getAdminById,
+  getUserById,
+} from "../../../../customers/users-list/core/_requests";
+
+const columns = [
+  "Customer Name",
+  "Subscription Code",
+  "Start Date",
+  "End Date",
+  "License Users",
+  "Invoice No",
+  "Invoice Date",
+  "Created By",
+  "Created DateTime",
+  "Customer Code",
+  "Plan Code",
+];
+
+const transformData = (
+  subscription: Subscription,
+  adminName: string | undefined,
+  companyName: string | undefined
+): { [key: string]: any } => {
+  return {
+    "Customer Name": companyName,
+    "Subscription Code": subscription.SUB_CODE,
+    "Start Date": subscription.SUB_STDT
+      ? new Date(subscription.SUB_STDT).toLocaleDateString()
+      : "",
+    "End Date": subscription.SUB_ENDT
+      ? new Date(subscription.SUB_ENDT).toLocaleDateString()
+      : "",
+    "License Users": subscription.LIC_USER,
+    "Invoice No": subscription.SUB_ORDN,
+    "Invoice Date": subscription.INV_DATE
+      ? new Date(subscription.INV_DATE).toLocaleDateString()
+      : "",
+    "Created By": adminName,
+    "Created DateTime": subscription.CREATED_AT
+      ? new Date(subscription.CREATED_AT).toLocaleString()
+      : "",
+    "Customer Code": subscription.CUS_CODE,
+    "Plan Code": subscription.PLA_CODE,
+  };
+};
 
 const SubscriptionsListToolbar: FC = () => {
   const { setItemIdForUpdate } = useListView();
-  const users = useQueryResponseData() as Subscription[];
+  const subscriptions = useQueryResponseData() as Subscription[];
   const isLoading = useQueryResponseLoading();
   const [csvData, setCsvData] = useState<string | null>(null);
+  const [adminNames, setAdminNames] = useState<{ [key: string]: string }>({});
+  const [companyNames, setCompanyNames] = useState<{ [key: string]: string }>(
+    {}
+  );
 
   const openAddSubscriptionModal = () => {
     setItemIdForUpdate(null);
   };
 
   useEffect(() => {
-    if (users && users.length > 0) {
-      const headers = Object.keys(users[0]);
-      const csvRows = [
-        headers.join(','), // CSV header row
-        ...users.map(user => 
-          headers.map(header => 
-            JSON.stringify(user[header as keyof Subscription])
-          ).join(',')
+    const fetchAdminNames = async () => {
+      const adminIds = Array.from(
+        new Set(
+          subscriptions
+            .map((subscription) => subscription.ad_id)
+            .filter((id) => id !== undefined)
         )
+      );
+      const adminNamesData: { [key: string]: string } = {};
+      await Promise.all(
+        adminIds.map(async (id) => {
+          const data = await getAdminById(id);
+          adminNamesData[id as string] = data.data.ad_name;
+        })
+      );
+      setAdminNames(adminNamesData);
+    };
+
+    if (subscriptions.length > 0) {
+      fetchAdminNames();
+    }
+  }, [subscriptions]);
+
+  useEffect(() => {
+    const fetchCompanyNames = async () => {
+      const companyIds = Array.from(
+        new Set(
+          subscriptions
+            .map((subscription) => subscription.CUS_CODE)
+            .filter((id) => id !== undefined)
+        )
+      );
+      const companyNamesData: { [key: string]: string } = {};
+      await Promise.all(
+        companyIds.map(async (id) => {
+          const data = await getUserById(id);
+          if (data && data.CUS_NAME)
+            companyNamesData[id as string] = data.CUS_NAME;
+        })
+      );
+      setCompanyNames(companyNamesData);
+    };
+
+    if (subscriptions.length > 0) {
+      fetchCompanyNames();
+    }
+  }, [subscriptions]);
+
+  useEffect(() => {
+    if (subscriptions.length > 0 && Object.keys(adminNames).length > 0) {
+      const csvRows = [
+        columns.join(","), // CSV header row
+        ...subscriptions.map((subscription) => {
+          const adminName = adminNames[subscription.ad_id as string];
+          const companyName = companyNames[subscription.CUS_CODE as string];
+          const transformedUser = transformData(
+            subscription,
+            adminName,
+            companyName
+          );
+          return columns
+            .map((column) => JSON.stringify(transformedUser[column]))
+            .join(",");
+        }),
       ];
-      const csv = csvRows.join('\n');
+      const csv = csvRows.join("\n");
       setCsvData(csv);
     }
-  }, [users]);
+  }, [subscriptions, adminNames, companyNames]);
 
   const exportCsv = () => {
     if (csvData) {
@@ -38,7 +146,7 @@ const SubscriptionsListToolbar: FC = () => {
       const link = document.createElement("a");
       link.setAttribute("href", url);
       link.setAttribute("download", "subscriptions_data.csv");
-      link.style.visibility = 'hidden';
+      link.style.visibility = "hidden";
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
@@ -46,7 +154,10 @@ const SubscriptionsListToolbar: FC = () => {
   };
 
   return (
-    <div className="d-flex justify-content-end" data-kt-user-table-toolbar="base">
+    <div
+      className="d-flex justify-content-end"
+      data-kt-user-table-toolbar="base"
+    >
       {/* begin::Export */}
       <button
         type="button"
