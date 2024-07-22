eb4971e67bbc0df7b63c4336db5f2e6f19ed19a6
jash413 <99473238+jash413@users.noreply.github.com>
2024-07-22 12:07:35 +0530

    fix: subscriptionProfile

--- src/app/modules/subscriptionprofile/ProfileHeader.tsx
+++ src/app/modules/subscriptionprofile/ProfileHeader.tsx
@@ -4,6 +4,8 @@ import { KTIcon } from "../../../_metronic/helpers";
 import { Content } from "../../../_metronic/layout/components/content";
 import { getEmployeeById } from "../employeeallotment/employees-list/core/_requests";
 import { getSubscriptionById } from "../subscriptions/subscriptions-list/core/_requests";
+import { getUserById } from "../customers/users-list/core/_requests";
+import moment from "moment";
 
 interface ProfileHeaderProps {
   id: string;
@@ -12,8 +14,9 @@ interface ProfileHeaderProps {
 const ProfileHeader: FC<ProfileHeaderProps> = ({ id }) => {
   const location = useLocation();
   const { pathname } = location;
-  const [subscription, setSubscription] = useState<any>({});
+  const [subscription, setSubscription] = useState<any>(null);
   const [employees, setEmployees] = useState<any>([]);
+  const [customer, setCustomer] = useState<any>({});
 
   useEffect(() => {
     getSubscriptionById(id).then((data) => {
@@ -23,6 +26,15 @@ const ProfileHeader: FC<ProfileHeaderProps> = ({ id }) => {
       setEmployees(data);
     });
   }, [id]);
+
+  useEffect(() => {
+    if (subscription) {
+      getUserById(subscription.CUS_CODE).then((data) => {
+        setCustomer(data);
+      });
+    }
+  }, [subscription]);
+
 
   return (
     <>
@@ -34,26 +46,26 @@ const ProfileHeader: FC<ProfileHeaderProps> = ({ id }) => {
                 <div className="d-flex justify-content-between align-items-start flex-wrap mb-2">
                   <div className="d-flex flex-column">
                     <div className="d-flex align-items-center mb-2">
-                      <a
-                        href="#"
+                      <Link
+                        to="#"
                         className="text-gray-800 text-hover-primary fs-2 fw-bolder me-1"
                       >
                         {subscription?.SUB_CODE}
-                      </a>
+                      </Link>
                     </div>
 
-                    {/* <div className="d-flex flex-wrap fw-bold fs-6 mb-4 pe-2">
-                      <a
-                        href={`tel:${customer?.PHO_NMBR}`}
+                    <div className="d-flex flex-wrap fw-bold fs-6 mb-4 pe-2">
+                      <Link
+                        to={`/customer-profile/${subscription?.CUS_CODE}`}
                         className="d-flex align-items-center text-gray-500 text-hover-primary me-5 mb-2"
                       >
                         <KTIcon
                           iconName="profile-circle"
                           className="fs-4 me-1"
                         />
-                        {customer?.PHO_NMBR}
-                      </a>
-                      <a
+                        {customer?.CUS_NAME}
+                      </Link>
+                      {/* <a
                         href="#"
                         className="d-flex align-items-center text-gray-500 text-hover-primary me-5 mb-2"
                       >
@@ -66,8 +78,8 @@ const ProfileHeader: FC<ProfileHeaderProps> = ({ id }) => {
                       >
                         <KTIcon iconName="sms" className="fs-4 me-1" />
                         {customer?.CUS_MAIL}
-                      </a>
-                    </div> */}
+                      </a> */}
+                    </div>
                   </div>
                 </div>
 
@@ -85,6 +97,24 @@ const ProfileHeader: FC<ProfileHeaderProps> = ({ id }) => {
                           Employees
                         </div>
                       </div>
+                      <div className="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
+                        <div className="d-flex align-items-center">
+                          <div className="fs-2 fw-bolder">
+                            {moment(subscription?.SUB_STDT).format("DD/MM/YYYY")}
+                          </div>
+                        </div>
+
+                        <div className="fw-bold fs-6 text-gray-500">Start Date</div>
+                        </div>
+                      <div className="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
+                        <div className="d-flex align-items-center">
+                          <div className="fs-2 fw-bolder">
+                            {moment(subscription?.SUB_ENDT).format("DD/MM/YYYY")}
+                          </div>
+                        </div>
+
+                        <div className="fw-bold fs-6 text-gray-500">End Date</div>
+                        </div>
                     </div>
                   </div>
                 </div>
