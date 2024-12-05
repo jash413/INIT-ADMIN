40b49a5dc99249a3706953f1410960aa4fb9e508
developeracc3012 <webwisedeveloper@yahoo.com>
2024-12-05 14:58:57 +0530

    fix: job post for candidates application

--- src/app/modules/jobportaljobpost/JobPostApplications.tsx
+++ src/app/modules/jobportaljobpost/JobPostApplications.tsx
@@ -3,8 +3,6 @@ import { KTCard, KTCardBody, KTIcon } from "../../../_metronic/helpers";
 import { Link, useParams } from "react-router-dom";
 import moment from "moment"; // For formatting the date
 import clsx from "clsx";
-import { toast } from "react-toastify";
-import { updateUserApprovalStatus } from "../jobportalemployer/employees-list/core/_requests";
 import { useGetJobApplicationsByJobId } from "./core/useGetJobApplicationsByJobId";
 import axios from "axios";
 import { REQ } from "../jobportalcandidate/core/_.request";
@@ -14,9 +12,8 @@ const JobPostApplications = () => {
 
     const [currentPage, setCurrentPage] = useState(1);
     const [searchTerm, setSearchTerm] = useState("");
-    const [statusUpdates, setStatusUpdates] = useState<Record<number, number>>({});
 
-    const { data, isLoading, refetch } = useGetJobApplicationsByJobId({
+    const { data, isLoading } = useGetJobApplicationsByJobId({
         page: currentPage,
         limit: 5,
         search: searchTerm,
@@ -52,24 +49,6 @@ const JobPostApplications = () => {
     };
 
     const paginationLinks = generatePaginationLinks();
-
-    const handleSwitchChange = async (loginId: number, isEnabled: boolean) => {
-        const newStatus = isEnabled ? 1 : 0;
-
-        setStatusUpdates((prev) => ({ ...prev, [loginId]: newStatus }));
-
-        try {
-            await updateUserApprovalStatus(loginId, newStatus);
-            toast.success("User approval status updated successfully.");
-            refetch();
-        } catch (error) {
-            setStatusUpdates((prev) => {
-                const updates = { ...prev };
-                delete updates[loginId];
-                return updates;
-            });
-        }
-    };
 
     const handleDownload = async (id: number, fileName: string) => {
         try {
@@ -113,35 +92,33 @@ const JobPostApplications = () => {
                             <div className="text-center">Loading...</div>
                         ) : records.length > 0 ? (
                             records.map((record: any) => {
-                                const userApprovalStatus =
-                                    statusUpdates[record.login_id] ?? record.Login?.user_approval_status;
                                 return (
                                     <div
                                         key={record.login_id}
                                         className="card border shadow-sm p-5 "
                                         style={{ width: "300px" }}
                                     >
-                                        <h5 className="card-title text-primary mb-3">
-                                            <Link className='text-hover-primary text-truncate' to={`/jobportal-candidate-management/${record.can_code}/candidate`}>
-                                                {record.can_name} - {record.can_code}
+                                        <h5 className="card-title text-primary mb-5">
+                                            <Link className='text-hover-primary text-truncate' to={`/jobportal-candidate-management/${record?.candidate?.can_code}/candidate`}>
+                                                {record?.candidate?.can_name} - {record?.candidate?.can_code}
                                             </Link>
                                         </h5>
                                         <p className="text-muted text-truncate">
-                                            <strong>Email:</strong> {record.can_email}
+                                            <strong>Email:</strong> {record?.candidate?.can_email}
                                         </p>
                                         <p className="text-muted text-truncate">
-                                            <strong>Mobile:</strong> {record.can_mobn}
+                                            <strong>Mobile:</strong> {record?.candidate?.can_mobn}
                                         </p>
                                         <p className="text-muted text-truncate">
-                                            <strong>Category:</strong> {record.job_category.cate_desc}
+                                            <strong>Category:</strong> {record?.candidate?.job_category.cate_desc}
                                         </p>
                                         <p className="text-muted text-truncate">
-                                            <strong>Applied on:</strong> {moment(record.createdAt).format("YYYY-MM-DD")}
+                                            <strong>Applied on:</strong> {moment(record.appliedAt).format("YYYY-MM-DD")}
                                         </p>
                                         <div className="mb-3">
-                                            {record?.can_resume ? (
+                                            {record?.can_resume?.can_resume ? (
                                                 <span
-                                                    onClick={() => handleDownload(record?.can_code, record.can_name)}
+                                                    onClick={() => handleDownload(record?.can_resume?.can_code, record?.can_resume?.can_name)}
                                                     style={{ cursor: "pointer" }}
                                                     className="text-primary fw-bold d-flex align-items-center"
                                                 >
@@ -150,23 +127,6 @@ const JobPostApplications = () => {
                                             ) : (
                                                 <span className="text-muted">No resume</span>
                                             )}
-                                        </div>
-                                        <div className="form-check form-switch">
-                                            <input
-                                                className="form-check-input"
-                                                type="checkbox"
-                                                id={`switch-${record.login_id}`}
-                                                checked={userApprovalStatus === 1}
-                                                onChange={(e) =>
-                                                    handleSwitchChange(record.login_id, e.target.checked)
-                                                }
-                                            />
-                                            <label
-                                                className="form-check-label"
-                                                htmlFor={`switch-${record.login_id}`}
-                                            >
-                                                {userApprovalStatus === 1 ? "Approved" : "Pending"}
-                                            </label>
                                         </div>
                                     </div>
                                 );
